5a47a411e7fe33eddff8cc68881ad6fc62d4870d
rubylo718 <rubylo718@gmail.com>
2022-04-12 10:53:33 +0800

    feat: add mongoose connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import exphbs from 'express-handlebars'
+import mongoose from 'mongoose'
 
 import { createRequire } from 'module' // Bring in the ability to create the 'require' method
 const require = createRequire(import.meta.url) // construct the require method
@@ -7,6 +8,16 @@ const restaurantList = require('./restaurant.json') // use the require method
 
 const app = express()
 const port = 3000
+mongoose.connect(process.env.MONGODB_URI_RES)
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected! good')
+})
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
